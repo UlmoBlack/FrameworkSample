7e2bbc2c3d11a168e4c9165dd9519f69ba0acfa8
UlmoBlack <ulmoblack@gmail.com>
2018-05-13 17:57:51 +0300

    EntityFramework Implementasyonu tamamlandı

---
+++ DevFramework/DevFramework.Core/DataAccess/EntityFramework/EfQuaryableRepository.cs
@@ -0,0 +1,45 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.EntityFramework
+{
+    public class EfQuaryableRepository<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private DbContext _context;
+        private IDbSet<T> _entities;
+
+        public EfQuaryableRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> Table => this.Entities; // Table çağırıldığında this.Entities return edilecek
+        //{
+        //    get
+        //    {
+        //        return this.Entities;
+        //    }
+        //}
+
+        protected virtual IDbSet<T> Entities
+        {
+
+            get { return _entities ?? (_entities = _context.Set<T>()); } // Eğer context null ise eski halini gönder
+            //get
+            //{
+            //    if(_entities == null)
+            //    {
+            //        _entities = _context.Set<T>();
+
+            //    }
+            //    return _entities;
+            //}
+        }
+
+    }
+}
